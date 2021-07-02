ccd545213a96462752caf39bfbd213be4470b800
Mário Sousa <mariojsxj@gmail.com>
2021-07-02 14:54:17 -0300

    Criando operação de instanciação.

--- src/main/java/com/mario/testProject/TestProjectApplication.java
+++ src/main/java/com/mario/testProject/TestProjectApplication.java
@@ -1,16 +1,35 @@
 package com.mario.testProject;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TestProjectApplication {
+import com.mario.testProject.domain.Categoria;
+import com.mario.testProject.repositores.CategoriaRepository;
 
+@SpringBootApplication
+public class TestProjectApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
+		
 	
-	
-	
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
--- src/main/java/com/mario/testProject/resources/CategoriaResource.java
+++ src/main/java/com/mario/testProject/resources/CategoriaResource.java
@@ -14,7 +14,7 @@ import com.mario.testProject.servisces.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
-public class CategoriaResources {
+public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
