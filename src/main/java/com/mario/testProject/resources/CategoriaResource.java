6d42d92503703b818efe013310b072c6e65eb8fd
Mário Sousa <mariojsxj@gmail.com>
2021-07-05 09:57:24 -0300

    Implement entity CategoriaDTO para listar toadas as categorias sem seus respectivos produtos.

--- src/main/java/com/mario/testProject/resources/CategoriaResource.java
+++ src/main/java/com/mario/testProject/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.mario.testProject.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mario.testProject.domain.Categoria;
+import com.mario.testProject.dto.CategoriaDTO;
 import com.mario.testProject.servisces.CategoriaService;
 
 @RestController
@@ -23,8 +26,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		Categoria obj = service.find(id);
-		
+		Categoria obj = service.find(id);		
 		return ResponseEntity.ok().body(obj);
 	}
 	
@@ -47,6 +49,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();	
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
