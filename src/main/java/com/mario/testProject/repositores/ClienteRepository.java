5b4d09b958ae332d2d357f0dd879fed6597a5a6d
Mário Sousa <mariojsxj@gmail.com>
2021-07-06 16:10:46 -0300

    Validação customizada; email não repetido na inserção de e-mail.

--- src/main/java/com/mario/testProject/repositores/ClienteRepository.java
+++ src/main/java/com/mario/testProject/repositores/ClienteRepository.java
@@ -2,12 +2,14 @@ package com.mario.testProject.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mario.testProject.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
 	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 	
 }
