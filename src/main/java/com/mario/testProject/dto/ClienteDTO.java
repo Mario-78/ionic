b5fa242377aaebc0033dcdf0858e11a76c880344
Mário Sousa <mariojsxj@gmail.com>
2021-07-06 17:36:39 -0300

    Validação customizada: Email não repetido na atualização.

--- src/main/java/com/mario/testProject/dto/ClienteDTO.java
+++ src/main/java/com/mario/testProject/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.mario.testProject.domain.Cliente;
+import com.mario.testProject.servisces.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable{
 
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/mario/testProject/servisces/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.mario.testProject.servisces.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.mario.testProject.domain.Cliente;
+import com.mario.testProject.dto.ClienteDTO;
+import com.mario.testProject.repositores.ClienteRepository;
+import com.mario.testProject.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email"," já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
