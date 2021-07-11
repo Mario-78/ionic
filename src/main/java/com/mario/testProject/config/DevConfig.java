a5ad842495024275cb9ad867df439839b4bf3559
Mário Sousa <mariojsxj@gmail.com>
2021-07-11 09:53:37 -0300

    Implementação do envio de email automátco de confirmação de pedido com gmail'

--- src/main/java/com/mario/testProject/config/DevConfig.java
+++ src/main/java/com/mario/testProject/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.mario.testProject.servisces.DBservice;
+import com.mario.testProject.servisces.EmailService;
+import com.mario.testProject.servisces.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,6 +33,11 @@ public class DevConfig {
 		
 		return true;
 		
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
