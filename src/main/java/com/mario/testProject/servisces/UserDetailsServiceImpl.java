35e77f133cb50127e780a8c4729498a2a15f02e7
Mário Sousa <mariojsxj@gmail.com>
2021-07-13 18:52:22 -0300

    Implementção, autenticação e geração do token JWT.

---
+++ src/main/java/com/mario/testProject/servisces/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.mario.testProject.servisces;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mario.testProject.domain.Cliente;
+import com.mario.testProject.repositores.ClienteRepository;
+import com.mario.testProject.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
