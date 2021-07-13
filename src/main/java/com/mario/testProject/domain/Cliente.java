7ec6e20e90fc54912ccd78a69602770c3cd81cc0
Mário Sousa <mariojsxj@gmail.com>
2021-07-12 21:12:39 -0300

    Salvando perfis do usuário na DB.

--- src/main/java/com/mario/testProject/domain/Cliente.java
+++ src/main/java/com/mario/testProject/domain/Cliente.java
@@ -6,18 +6,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mario.testProject.domain.enums.Perfil;
 import com.mario.testProject.domain.enums.TipoCliente;
 
 @Entity
@@ -46,12 +49,16 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
-		
+		addPerfil(Perfil.CLIENTE);		
 	}
 
 	public Cliente(Integer id, String name, String email, String cpfOuCnpj, TipoCliente tipoCliente, String senha) {
@@ -62,6 +69,7 @@ public class Cliente implements Serializable{
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipoCliente = (tipoCliente==null) ? null : tipoCliente.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -136,6 +144,13 @@ public class Cliente implements Serializable{
 		this.senha = senha;
 	}
 
+	public Set<Perfil> getPerfil(){
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/mario/testProject/servisces/DBservice.java
+++ src/main/java/com/mario/testProject/servisces/DBservice.java
@@ -20,6 +20,7 @@ import com.mario.testProject.domain.PagamentoComCartao;
 import com.mario.testProject.domain.Pedido;
 import com.mario.testProject.domain.Produto;
 import com.mario.testProject.domain.enums.EstadoPagamento;
+import com.mario.testProject.domain.enums.Perfil;
 import com.mario.testProject.domain.enums.TipoCliente;
 import com.mario.testProject.repositores.CategoriaRepository;
 import com.mario.testProject.repositores.CidadeRepository;
@@ -126,12 +127,18 @@ public class DBservice {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOA_FISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "rua Flores", "300", "Apto 203", "Jardim", "38777012", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
