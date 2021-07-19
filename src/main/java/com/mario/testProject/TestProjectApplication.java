c7ce0bc183acb005cfdaa8037730a081211441f2
Mário Sousa <mariojsxj@gmail.com>
2021-07-19 16:54:53 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/mario/testProject/TestProjectApplication.java
+++ src/main/java/com/mario/testProject/TestProjectApplication.java
@@ -1,12 +1,17 @@
 package com.mario.testProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mario.testProject.servisces.S3Service;
+
 @SpringBootApplication
 public class TestProjectApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
@@ -16,6 +21,6 @@ public class TestProjectApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("H:\\G3\\Saved Pictures\\ima.png");
 	}
 }
